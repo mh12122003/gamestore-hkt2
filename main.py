02e2acd39a6a0cc195164c7ff18faf6fbfae6d10
Khangdepzai <khangtgr@gmail.com>
2022-11-05 13:51:20 +0700

    add more links

--- main.py
+++ main.py
@@ -7,12 +7,20 @@ app = Flask(__name__)
 
 
 # === SHOW screen from index ===============
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/create_account", methods=['GET', 'POST'])
 # PURPOSE: to show content in the URL: '/', which also means 'localhost:5000' as defaut
-def show_screen():
+def show_screen_create_account():
     # PURPOSE: the frontend/UI application would be 'html' file
     # 'render_template': use the file as the UI/frontend of the app
-    return render_template('index.html')
+    return render_template('create_account.html')
+
+@app.route("/login", methods=['GET', 'POST'])
+def show_screen_login():
+    return render_template('login.html')
+
+@app.route("/homepage", methods=['GET', 'POST'])
+def show_screen_homepage():
+    return render_template('homepage.html')
 
 
 if __name__ == "__main__":
